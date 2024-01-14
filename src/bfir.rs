aee304fdc7c6a51902132d5e2723f9eeefd1ac59
kusunoki0308/kusunoki0308 <1505979366@qq.com>
2024-01-14 19:46:11 +0800

    add runtime error: pointer overflow

--- src/bfir.rs
+++ src/bfir.rs
@@ -9,11 +9,24 @@ pub enum CompileErrorType {
     RightBracketError,
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum RuntimeErrorType {
+    #[error("Pointer overflow")]
+    PointerOverFlowError,
+}
+
 #[derive(Debug)]
-pub struct CompileError {
+pub enum BfErrorType {
+    CompileError(CompileErrorType),
+    RuntimeError(RuntimeErrorType)
+}
+
+
+#[derive(Debug)]
+pub struct BfError {
     pub line: u32,
     pub col: u32,
-    pub error_type: CompileErrorType
+    pub error_type: BfErrorType
 }
 
 #[derive(Debug)]
@@ -29,9 +42,9 @@ pub enum BfIR {
 }
 
 impl BfIR {
-    pub fn run(str_tokens: &str) -> Result<Vec<char>, CompileError> {
+    pub fn run(str_tokens: &str) -> Result<Vec<char>, BfError> {
 
-        let code: Vec<BfIR>;
+        let code: Vec<(BfIR, u32, u32)>;
         let jmp_table:HashMap<usize, usize>;
         match Self::build(str_tokens) {
             Ok((res_token, res_table)) => {
@@ -43,17 +56,36 @@ impl BfIR {
             }
         };
 
+        const MEMORY_SIZE: usize = 512;
         let mut display: Vec<char> = Vec::new();
-        let mut memory: [u8; 511] = [0; 511];
-        let mut data_p: usize = 256;
+        let mut memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
+        let mut data_p: usize = MEMORY_SIZE/2;
         let mut code_p: usize = 0;
 
         while code_p as usize != code.len() {
-            match code[code_p] {
-                BfIR::BfAdd(num) => memory[data_p] += num,
-                BfIR::BfSub(num) => memory[data_p] -= num,
-                BfIR::BfNext(num) => data_p += num as usize, 
-                BfIR::BfPre(num) => data_p -= num as usize,
+            match code[code_p].0 {
+                BfIR::BfAdd(num) => {memory[data_p] = memory[data_p].wrapping_add(num);},
+                BfIR::BfSub(num) => {memory[data_p] = memory[data_p].wrapping_sub(num);},
+                BfIR::BfNext(num) => {
+                    data_p += num as usize;
+                    if data_p >= MEMORY_SIZE {
+                        return Err(BfError{
+                            line: code[code_p].1,
+                            col: code[code_p].2,
+                            error_type: BfErrorType::RuntimeError(RuntimeErrorType::PointerOverFlowError)
+                        });
+                    } 
+                }, 
+                BfIR::BfPre(num) => {
+                    if let None = data_p.checked_sub(num as usize) {
+                        return Err(BfError{
+                            line: code[code_p].1,
+                            col: code[code_p].2,
+                            error_type: BfErrorType::RuntimeError(RuntimeErrorType::PointerOverFlowError)
+                        });
+                    }
+                    data_p -= num as usize;
+                },
                 BfIR::BfJz => {
                     if memory[data_p] == 0 {
                         code_p = jmp_table[&code_p];
@@ -83,35 +115,36 @@ impl BfIR {
 
     }
 
-    pub fn build(str_tokens: &str) -> Result<(Vec<BfIR>, HashMap<usize, usize>), CompileError> {
+    pub fn build(str_tokens: &str) -> Result<(Vec<(BfIR, u32, u32)>, HashMap<usize, usize>), BfError> {
         let mut line_p = 1;
         let mut col_p = 0;
-        let mut tokens: Vec<BfIR> = Vec::new();
+        let mut tokens: Vec<(BfIR, u32, u32)> = Vec::new();
         let mut sta: Vec<(usize, u32, u32)> = Vec::new();
         let mut jmp_table: HashMap<usize, usize> = HashMap::new();
 
         for ch in str_tokens.chars() {
+            col_p += 1;
             match ch {
-                '+' => tokens.push(BfIR::BfAdd(1)),
-                '-' => tokens.push(BfIR::BfSub(1)),
-                '>' => tokens.push(BfIR::BfNext(1)),
-                '<' => tokens.push(BfIR::BfPre(1)),
+                '+' => tokens.push((BfIR::BfAdd(1), line_p, col_p)),
+                '-' => tokens.push((BfIR::BfSub(1), line_p, col_p)),
+                '>' => tokens.push((BfIR::BfNext(1), line_p, col_p)),
+                '<' => tokens.push((BfIR::BfPre(1), line_p, col_p)),
                 '[' => {
                     sta.push((tokens.len(), line_p, col_p));
-                    tokens.push(BfIR::BfJz);
+                    tokens.push((BfIR::BfJz, line_p, col_p));
                 },
                 ']' => {
-                    let jz = sta.pop().ok_or(CompileError{
+                    let jz = sta.pop().ok_or(BfError{
                         line: line_p,
                         col: col_p,
-                        error_type: CompileErrorType::RightBracketError
+                        error_type: BfErrorType::CompileError(CompileErrorType::RightBracketError)
                     })?;
-                    tokens.push(BfIR::BfJnz);
+                    tokens.push((BfIR::BfJnz, line_p, col_p));
                     jmp_table.insert(jz.0, tokens.len() - 1);
                     jmp_table.insert(tokens.len() - 1, jz.0);
                 },
-                '.' => tokens.push(BfIR::BfWrite),
-                ',' => tokens.push(BfIR::BfRead),
+                '.' => tokens.push((BfIR::BfWrite, line_p, col_p)),
+                ',' => tokens.push((BfIR::BfRead, line_p, col_p)),
                 '\n' => {
                     col_p = 0;
                     line_p += 1;
@@ -121,10 +154,10 @@ impl BfIR {
         }
 
         if sta.len() > 0 {
-            return Err(CompileError{
+            return Err(BfError{
                 line: sta[0].1,
                 col: sta[0].2,
-                error_type: CompileErrorType::LeftBracketError
+                error_type: BfErrorType::CompileError(CompileErrorType::LeftBracketError)
             });
         }    
 
